db3061772601c690ff79e4d31ea9175a3b6680c2
Ruslan Mochulskyy <mochulskyy.ruslan@gmail.com>
2020-04-11 17:50:47 +0200

    POST now uploads the URL-parameter to the database

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
 from flask_cors import CORS
 app = Flask(__name__)
 # Turn on Cross Origin Resource Sharing (CORS) for all requests
 CORS(app)
 
-
+#'POST' or 'GET' request under the URL => http://127.0.0.1:5000/data
 @app.route('/data', methods=['GET', 'POST'])
 def login():
+    #Just test data
     data = [
         {"content":"mochulskyy.com"},
         {"content":"some other "},
         {"content":"just"}
     ]
 
+    #Request 'GET'
     if request.method == 'GET':
         return jsonify(data);
 
+    #Request 'POST'
     elif request.method == 'POST':
         return jsonify(content= 'mochulskyy.com', time= '10:00')
 
